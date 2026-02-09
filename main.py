0bde68c179a6530f16c977e08f4ed222e8a1e789
uzair <uzairchini555@gmail.com>
2026-02-09 21:27:09 +0500

    Test CD pipeline

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def home():
-	return {"message": "Hello from Kubernetes Project4 CI pipelien and push"}
+	return {"message": "CD Pipeline Test"}

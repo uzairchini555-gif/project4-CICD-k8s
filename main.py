116b02cecdd52cb7528cf995b6ee49c8122c49e0
uzair <uzairchini555@gmail.com>
2026-02-14 00:09:25 +0500

    Final Prodouction push

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def home():
-	return {"Production Rolling Update Test""}
+	return {"Production Rolling Update Test"}
 @app.get("/health")
 def health():
 	return {"status": "ok"}
